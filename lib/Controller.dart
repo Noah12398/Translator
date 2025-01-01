cdf95a2ee348b41ce6f38df866a822066194831b
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2025-01-01 19:14:16 +0530

    DropDown

--- lib/Controller.dart
+++ lib/Controller.dart
@@ -5,18 +5,30 @@ import 'package:translator/translator.dart';
 class TranslatorController extends GetxController {
   String inputText = '';
   String translatedText = '';
+
+  String selectedItem = 'en';
+  String selectedItem2 = 'en';
   TextEditingController inputTextController = TextEditingController();
   TextEditingController translatedTextController = TextEditingController();
-  final GoogleTranslator translator =GoogleTranslator();
+  final GoogleTranslator translator = GoogleTranslator();
 
   Future<void> translateText() async {
-  try {
-    final translation = await translator.translate(inputTextController.text, to: 'en');
-    translatedText = translation.text;
-    translatedTextController.text = translatedText;
-  } catch (e) {
-    print('Error during translation: $e');
+    try {
+      final translation =
+          await translator.translate(inputTextController.text,from: selectedItem2 ,to: selectedItem);
+      translatedText = translation.text;
+      translatedTextController.text = translatedText;
+    } catch (e) {
+      print('Error during translation: $e');
+    }
   }
-}
 
+  changelanguage(String newValue) {
+    selectedItem = newValue;
+    update();
+  }
+  changelanguage2(String newValue) {
+    selectedItem2 = newValue;
+    update();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  MyHomePage(title: 'Translator App'),
+      home: MyHomePage(title: 'Translator App'),
     );
   }
 }
@@ -26,8 +25,9 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key, required this.title});
   final FocusNode inputFocusNode = FocusNode();
-
   final String title;
+  final List<String> _items = ['en', 'fr', 'es', 'de','ur','hi'];
+
   @override
   Widget build(BuildContext context) {
     TranslatorController controller = Get.put(TranslatorController());
@@ -36,43 +36,91 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
-              controller: controller.inputTextController,
-              focusNode: inputFocusNode,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Enter text to translate"),
-              maxLines: 6,
+      body: GetBuilder<TranslatorController>(
+        builder: (_) {
+          return (Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: controller.inputTextController,
+                  focusNode: inputFocusNode,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Enter text to translate"),
+                  maxLines: 6,
+                ),
+                DropdownButton<String>(
+                  value: controller.selectedItem2,
+                  hint: const Text('Language to be translated'),
+                  onChanged: (String? newValue) {
+                    if(newValue!=null){
+                    controller.changelanguage2(newValue);}
+                  },
+                  items: _items.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    );
+                  }).toList(),
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.green),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.red,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    inputFocusNode.requestFocus();
+                    controller.translateText();
+                  },
+                  child: const Text('Translate'),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                TextField(
+                  controller: controller.translatedTextController,
+                  focusNode: inputFocusNode,
+                  decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: controller.translatedTextController.text),
+                  maxLines: 6,
+                ),
+                DropdownButton<String>(
+                  value: controller.selectedItem,
+                  hint: const Text('Language to translated to'),
+                  onChanged: (String? newValue) {
+                    if(newValue!=null){
+                    controller.changelanguage(newValue);}
+                  },
+                  items: _items.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    );
+                  }).toList(),
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.green),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 5,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                inputFocusNode.requestFocus();
-                controller.translateText();
-              },
-              child: const Text('Translate'),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            TextField(
-              controller: controller.translatedTextController,
-              focusNode: inputFocusNode,
-              decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  labelText: controller.translatedTextController.text),
-              maxLines: 6,
-            )
-          ],
-        ),
+          ));
+        },
       ),
     );
   }
